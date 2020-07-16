c21ba447f2effb1f520f3bc471c0c73f325e514e
mike_visser <mpviss@gmail.com>
2020-07-16 17:34:49 -0400

    email form complete

--- src/Forms/Form.js
+++ src/Forms/Form.js
@@ -1,14 +1,43 @@
 import React from 'react'
+import './Form.css'
 
 
 
 const Form = (props) => {
+
+    let classes = ["input"]
+
+    if(props.medium){
+        classes.push("medium")
+    }
+    if(props.large){
+        classes.push("large")
+    }
+
+    const emailForm = props => {
+        return <div>
+            <p>{props.placeholder}</p>
+            <input type={props.type} placeholder={props.placeholder} className={classes.join(" ")} />
+        </div>
+    }
+
+    const selectForm = props => {
+        return <select>
+            <option></option>
+            <option></option>
+            <option></option>
+            <option></option>
+            <option></option>
+        </select>
+    }
+
     return (
-        <form>
-            <p>{props.label}sdsd</p>
-            <input type={props.type} placeholder={props.placeholder} />
+        <form className="form">
+            {props.type=="email" ? emailForm(props) :null}
+            {props.type=="select" ? selectForm(props) :null}
         </form>
     )
 }
 
 export default Form
+
--- src/Forms/Form.stories.js
+++ src/Forms/Form.stories.js
@@ -3,6 +3,22 @@ import { storiesOf } from '@storybook/react';
 
 import Form from './Form'
 
+let options = ["Pizza", "Wings", "Burgers", "Fries", "Yes"]
+
 storiesOf('Form', module)
 
-.add('Default', () => <Form />)
+//EMAIL INPUT
+    
+    .add('Email Small', () => <Form type="email" placeholder="Email" />)
+    .add('Email Medium', () => <Form type="email" medium placeholder="Email" />)
+    .add('Email Large', () => <Form type="email" large placeholder="Email" />)
+
+//SELECT INPUT
+
+    .add('Select Small', () => <Form type="select" placeholder="Email" />)
+    .add('Select Medium', () => <Form type="select" medium placeholder="Email" />)
+    .add('Select Large', () => <Form type="select" large placeholder="Email" />)
+
+    .add('Select Filled Small', () => <Form type="select" placeholder="Email" />)
+    .add('Select Filled Medium', () => <Form type="select" medium placeholder="Email" />)
+    .add('Select Filled Large', () => <Form type="select" large placeholder="Email" />)
